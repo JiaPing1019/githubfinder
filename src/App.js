276c5940aa0a4edd1c51ce16dcb9f9260c6940d7
JiaPing <jumpping4@gmail.com>
2019-10-06 17:57:40 +0900

    Add Home and NotFound components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import User from './components/users/User';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
@@ -22,22 +22,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props =>(
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
-                <Route
-                  exact
-                  path='/user/:login'
-                  component={User}
-                />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Search from '../users/Search';
+import Users from '../users/Users';
+ 
+const NotFound = () => (
+  <div>
+    <h1>Not Found</h1>
+    <p className='lead'>The page you are looking for does not exist...</p>
+  </div>
+);
+
+export default NotFound;
