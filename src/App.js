fd5af62d23232dedf7b2a780763e1a46cf601088
JiaPing <jumpping4@gmail.com>
2019-10-06 15:37:42 +0900

    Moving User state to context

--- src/App.js
+++ src/App.js
@@ -11,21 +11,9 @@ import GithubState from './context/github/GithubState'
 import './App.css';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-  // Get single Github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUser(res.data);
-    setLoading(false);
-  };
 
   // Get users repos
   const getUserRepos = async (username) => {
@@ -34,12 +22,6 @@ const App = () => {
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     setRepos(res.data);
-    setLoading(false);
-  };
-
-  // Clear users from state
-  const clearUsers = () => {
-    setUsers([]);
     setLoading(false);
   };
 
@@ -63,8 +45,6 @@ const App = () => {
                 render={props =>(
                   <Fragment>
                     <Search
-                      clearUsers={clearUsers}
-                      showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
                     />
                     <Users />
@@ -78,11 +58,8 @@ const App = () => {
                 render={props =>(
                   <User
                     {...props}
-                    getUser={getUser}
                     getUserRepos={getUserRepos}
-                    user={user}
                     repos={repos}
-                    loading={loading}
                    />
                 )}
               />
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -14,10 +14,22 @@ export default (state, action) => {
         users: action.payload,
         loading: false
       };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
+      };
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false
+      };
     case SET_LOADING:
       return {
         ...state,
-        loading: false
+        loading: true
       };
     default:
       return state;
