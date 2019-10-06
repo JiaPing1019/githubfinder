dc7d774f23946f529c71b3f0fb581db8811ceff1
JiaPing <jumpping4@gmail.com>
2019-10-06 17:37:00 +0900

    Add Alert Context

--- src/App.js
+++ src/App.js
@@ -6,48 +6,43 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import User from './components/users/User';
 import About from './components/pages/About';
+
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+
 import './App.css';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Show Alert
-  const showAlert = (msg, type) => {
-    setAlert({msg, type});
-    setTimeout(() => setAlert(null), 3000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props =>(
-                  <Fragment>
-                    <Search
-                      setAlert={showAlert}
-                    />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                component={User}
-              />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={props =>(
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route
+                  exact
+                  path='/user/:login'
+                  component={User}
+                />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 }
