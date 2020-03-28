217719d898e926fda94cb7dab78946c17ca526f3
JiaPing <jumpping4@gmail.com>
2020-03-28 18:31:06 +0900

    Install material-ui

--- src/components/repos/RepoLists.js
+++ src/components/repos/RepoLists.js
@@ -1,30 +1,43 @@
-import React, { useContext } from 'react';
+import React, {useContext} from 'react';
 import RepoItem from './RepoItem';
 import Spinner from '../layout/Spinner';
 import GithubContext from '../../context/github/githubContext';
 
+import {makeStyles} from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+
 const RepoLists = () => {
   const githubContext = useContext(GithubContext);
+  const {loading, repos} = githubContext;
+  const classes = useStyles();
 
-  const { loading, repos } = githubContext;
-
-  if(loading) {
-    return <Spinner />
+  if (loading) {
+    return <Spinner />;
   } else {
     return (
-      <div style={userStyle}>
-        {repos.map(repo => (
-           <RepoItem key={repo.id} repo= {repo} />
-        ))}
+      <div className={classes.root}>
+        <List height={400} width={300}>
+          {repos.map(repo => (
+            <RepoItem key={repo.id} repo={repo} />
+          ))}
+        </List>
       </div>
-    )
+    );
   }
-}
+};
 
-const userStyle = {
-  display: 'griduserStyle',
-	gridTemplatecolumns: 'repeat(3, 1fr)',
-	gridGap: '1rem' 
-}
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    height: 400,
+    maxWidth: 300,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+// const userStyle = {
+//   display: 'griduserStyle',
+// 	gridTemplatecolumns: 'repeat(3, 1fr)',
+// 	gridGap: '1rem'
+// }
 
 export default RepoLists;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,16 @@
-import React, { Fragment, useEffect, useContext } from 'react';
+import React, {Fragment, useEffect, useContext} from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import GithubContext from '../../context/github/githubContext';
+import {makeStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const User = ({ match }) => {
+const User = ({match}) => {
   const githubContext = useContext(GithubContext);
-  const { user, loading, getUser, repos, getUserRepos } = githubContext
+  const {user, loading, getUser, repos, getUserRepos} = githubContext;
+  const classes = useStyles();
 
   useEffect(() => {
     getUser(match.params.login);
@@ -27,70 +31,107 @@ const User = ({ match }) => {
     following,
     public_repos,
     public_gists,
-    hireable
+    hireable,
   } = user;
 
-  if (loading) return <Spinner />
+  if (loading) return <Spinner />;
 
-  return <Fragment>
-    <Link to='/' className='btn btn-light'>
-      Back to search
-    </Link>
-    Hirable:{' '}
-    {hireable ? (
-      <i className='fas fa-check text-success' />
-    ) : (
-      <i className='fas fa-items-circle text-danger' />
-    )}
-    <div className='card grid-2'>
-      <div className='all-center'>
-        <img
-          src={avatar_url}
-          className='round-img'
-          alt=''
-          style={{ width: '150px'}}
-        />
-        <h1>{name}</h1>
-        <p>Location: {location}</p>
+  return (
+    <Fragment>
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Link to="/" className="btn btn-light">
+              Back to search
+            </Link>
+            Hirable:{' '}
+            {hireable ? (
+              <i className="fas fa-check text-success" />
+            ) : (
+              <i className="fas fa-items-circle text-danger" />
+            )}
+            <div className="card grid-2">
+              <div className="all-center">
+                <img
+                  src={avatar_url}
+                  className="round-img"
+                  alt=""
+                  style={{width: '150px'}}
+                />
+                <h1>{name}</h1>
+                <p>Location: {location}</p>
+              </div>
+              <div>
+                {bio && (
+                  <Fragment>
+                    <h3>Bio</h3>
+                    <p>{bio}</p>
+                  </Fragment>
+                )}
+                <a href={html_url} className="btn btn-dark my-1">
+                  Visit Github Profile
+                </a>
+                <ul>
+                  <li>
+                    {login && (
+                      <Fragment>
+                        <strong>Username: </strong> {login}
+                      </Fragment>
+                    )}
+                  </li>
+
+                  <li>
+                    {company && (
+                      <Fragment>
+                        <strong>Company: </strong> {company}
+                      </Fragment>
+                    )}
+                  </li>
+
+                  <li>
+                    {blog && (
+                      <Fragment>
+                        <strong>Blog: </strong> {blog}
+                      </Fragment>
+                    )}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="card text-center">
+              <div className="badge badge-primary">Followers: {followers}</div>
+              <div className="badge badge-success">Following {following}</div>
+              <div className="badge badge-danger">
+                Public Repos: {public_repos}
+              </div>
+              <div className="badge badge-dark">
+                Public Gists: {public_gists}
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <Repos repos={repos} />
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs</Paper>
+          </Grid>
+        </Grid>
       </div>
-      <div>
-        {bio && <Fragment>
-          <h3>Bio</h3>
-          <p>{bio}</p>
-        </Fragment>}
-        <a href={html_url} className='btn btn-dark my-1'>
-          Visit Github Profile
-        </a>
-        <ul>
-          <li>
-            {login && <Fragment>
-              <strong>Username: </strong> {login}
-            </Fragment>}
-          </li>
+    </Fragment>
+  );
+};
 
-          <li>
-            {company && <Fragment>
-              <strong>Company: </strong> {company}
-            </Fragment>}
-          </li>
-
-          <li>
-            {blog && <Fragment>
-              <strong>Blog: </strong> {blog}
-            </Fragment>}
-          </li>
-
-        </ul>
-      </div>
-    </div>
-    <div className='card text-center'>
-      <div className='badge badge-primary'>Followers: {followers}</div>
-      <div className='badge badge-success'>Following {following}</div>
-      <div className='badge badge-danger'>Public Repos: {public_repos}</div>
-      <div className='badge badge-dark'>Public Gists: {public_gists}</div>
-    </div>
-    <Repos repos={repos} />
-  </Fragment>
-}
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default User;
